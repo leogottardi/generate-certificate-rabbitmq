f3b82d6473f915190b20329f983096f1a91762c8
Leonardo Gottardi <leonardo.gottardi@stone.com.br>
2023-02-08 08:33:10 -0300

    feat: export singleton of generateCertificateService

--- certificate/src/infra/containers/index.ts
+++ certificate/src/infra/containers/index.ts
@@ -1,9 +1,5 @@
-import { IRabbitMQ } from '../../queues/interfaces'
-import { consumeQueues } from '../../queues/queues'
-import { RabbitMQ } from '../../queues/rabbitmq'
 import { GenerateCertificateService } from '../../services/generate-certificate'
 
-const rabbitMQ = new RabbitMQ() as IRabbitMQ
-consumeQueues(rabbitMQ)
-const generateCertificateService = new GenerateCertificateService(rabbitMQ)
+const generateCertificateService = new GenerateCertificateService()
+
 export { generateCertificateService }
