1af65123bee55df19e0c567f78ddd7b745611adb
Leonardo Gottardi <leonardo.gottardi@stone.com.br>
2023-02-08 08:32:12 -0300

    feat: add startQueues in generate_certificate api

--- certificate/src/queues/queues.ts
+++ certificate/src/queues/queues.ts
@@ -1,16 +1,10 @@
-import { ConsumeMessage } from 'amqplib'
 import { IRabbitMQ } from './interfaces'
 
-export async function consumeQueues(rabbitMQ: IRabbitMQ) {
+export async function startQueues(rabbitMQ: IRabbitMQ) {
     await rabbitMQ.start()
     await rabbitMQ.assertExchange('api_exchange', 'direct')
     await rabbitMQ.assertQueue('generate_certificate')
     await rabbitMQ.bindQueue('generate_certificate', 'api_exchange', 'generate_certificate')
     await rabbitMQ.assertQueue('confirm_certificate')
     await rabbitMQ.bindQueue('confirm_certificate', 'api_exchange', 'confirm_certificate')
-    await rabbitMQ.consumeQueue('generate_certificate', (message: ConsumeMessage) => {
-        if (!message) return
-        console.log(JSON.parse(message.content.toString()))
-        rabbitMQ.channel?.ack(message)
-    })
 }
